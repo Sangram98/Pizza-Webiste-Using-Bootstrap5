f016a43fcebe56e5ff45445186270df711af6060
sangram <sks98.sahoo@gmail.com>
2022-02-14 20:07:29 +0530

    initial commit

---
+++ javascript/main.js
@@ -0,0 +1,20 @@
+// scrollbar navbar
+let nav=document.querySelector(".navigation-wrap");
+window.onscroll=function(){
+    if(document.documentElement.scrollTop >20){
+        nav.classList.add("scroll-on")
+    }else{
+        nav.classList.remove("scroll-on")
+
+    }
+}
+
+// nav hide on mobile
+
+let navbar=document.querySelectorAll(".nav-link");
+let navcollapse=document.querySelector(" .navbar-collapse .collapse");
+navbar.forEach(function (a){
+    a.addEventListener("click",function(){
+        navcollapse.classList.remove("show");
+    })
+})
